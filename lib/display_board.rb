5dd083c2dbac3ef2be94670ebe8496585388855d
Harry Dibden <harry-dibden@hotmail.com>
2018-07-16 14:39:08 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,3 +5,6 @@ def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts sep
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts sep
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
